d50a8d760dacd5cb370386f6149fd614941c69e1
Wojciech Zalewski <wojtek@kropka.net>
2016-05-30 16:54:31 +0200

    Cleaned up a bit (#1)

--- app/src/app.php
+++ app/src/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use Silex\Application;
-use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
@@ -11,13 +10,6 @@ $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
-$app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
-
-$app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
-    // add custom globals, filters, tags, ...
-
-    return $twig;
-}));
 
 return $app;
