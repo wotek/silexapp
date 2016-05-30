04d7b9427491c6d1887848765d212ce89af59de9
Wojtek Zalewski <wojtek@neverbland.com>
2016-05-30 16:47:25 +0200

    Cleaned up a bit

--- app/src/controllers.php
+++ app/src/controllers.php
@@ -8,24 +8,22 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
+/**
+ * Main route
+ */
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array());
+    return new Response('Hello world');
 })
 ->bind('homepage')
 ;
 
+/**
+ * Error route
+ */
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
         return;
     }
 
-    // 404.html, or 40x.html, or 4xx.html, or error.html
-    $templates = array(
-        'errors/'.$code.'.html',
-        'errors/'.substr($code, 0, 2).'x.html',
-        'errors/'.substr($code, 0, 1).'xx.html',
-        'errors/default.html',
-    );
-
-    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+    throw $e;
 });
